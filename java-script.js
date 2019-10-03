5eb09421ad78ae85995fdcdc2a45fca46a235ba8
Anastasiya Pozyomina <44189472+AnastasiyaPozyomina@users.noreply.github.com>
2019-10-03 23:16:52 +0700

    "Finished layout page"

--- java-script.js
+++ java-script.js
@@ -1,15 +1,12 @@
 $('.sl').slick();
 
-$('.sl-2').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
 
-var h1 = document.createElement('h1');
-h1.className = "header-title";
-h1.innerHTML="Cleaning Yours House Make World & Clean Mine";
-document.body.insertBefore(h1, document.body.firstChild);
-setTimeout(function () {
-    h1.parentNode.removeChild(h1);
-    },2000);
+
+
+//  галерея
+var menuElem = document.getElementById('main');
+var titleElem = menuElem.querySelector('.title');
+
+titleElem.onclick = function() {
+  menuElem.classList.toggle('open');
+};
