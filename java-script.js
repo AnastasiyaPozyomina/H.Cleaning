229605e2e8a78e754cc847499c32ddd54387825e
Anastasiya Pozyomina <44189472+AnastasiyaPozyomina@users.noreply.github.com>
2020-01-07 04:46:10 +0700

    slick slider

--- java-script.js
+++ java-script.js
@@ -1,7 +1,20 @@
-$('.sl').slick();
+$('.header-carusel').slick({
+  autoplay:true,
+  autoplaySpeed:3000,
+  dots:true
+ 
+});
 
 
-
+$('.gallery').slick({
+  slidesToShow: 3, //сколько слайдов показывать в карусели
+  slidesToScroll: 3, // сколько слайдов прокручивать за раз
+  autoplay:true,
+  autoplaySpeed:2000,
+  nextArrow: '<div class="sprite-next"></div>', // Кастомная стрелка "далее"
+  prevArrow: '<div class="sprite-prev"></div>' // Кастомная стрелка "назад"
+ 
+});
 
 //  галерея
 var menuElem = document.getElementById('main');
@@ -10,3 +23,5 @@ var titleElem = menuElem.querySelector('.title');
 titleElem.onclick = function() {
   menuElem.classList.toggle('open');
 };
+
+
